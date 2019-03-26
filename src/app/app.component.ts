f0f939deac1eea67ec5fb67814ba05968246e26e
Douglas Garrigan <douglasgarrigan@Douglass-Mac-Pro.local>
2019-03-26 07:07:12 -0400

    fc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,84 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rogers';
+
+  days = ['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA'];
+
+  weather = [
+    {
+      'id': 1907296,
+      'name': 'Toronto',
+      'current': [
+        {
+          'id': 800,
+          'main': 'Clear',
+          'description': 'clear sky',
+          'icon': '01n'
+        }
+      ],
+      'upcoming': [
+        {
+          'SU': {
+            'icon': '',
+            'temp': 31
+          },
+          'MO': {
+            'icon': '',
+            'temp': 31
+          },
+          'TU': {
+            'icon': '',
+            'temp': 31
+          },
+          'WE': {
+            'icon': '',
+            'temp': 31
+          },
+          'TH': {
+            'icon': '',
+            'temp': 31
+          },
+          'FR': {
+            'icon': '',
+            'temp': 31
+          },
+          'SA': {
+            'icon': '',
+            'temp': 31
+          }
+        }
+      ]
+    },
+    {
+      'id': 1907296,
+      'name': 'Vancouver',
+      'cod': {
+        'lon': 139.01,
+        'lat': 35.02
+      },
+      'weather': [
+        {
+          'id': 800,
+          'main': 'Clear',
+          'description': 'clear sky',
+          'icon': '01n'
+        }
+      ]
+    }
+  ];
+
+
+
+  myInterval = 1500;
+  activeSlideIndex = 0;
+
+  slides = [
+    { image: 'assets/img1.jpg' },
+    { image: 'assets/img1.jpg' },
+    { image: 'assets/img1.jpg' },
+  ];
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AccordionModule } from 'ngx-bootstrap/accordion';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatCardModule, MatIconModule, MatToolbarModule, MatButtonModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +15,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AccordionModule.forRoot(),
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    CarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
